9b56e2e04b3a19adde4b9a39a466dcacb5b5cc84
Daniel Fenjves <fenjves.daniel@gmail.com>
2014-05-13 15:09:29 -0400

    built form for statuses and filled out controller

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -4,8 +4,32 @@ class StatusesController < ApplicationController
 		@statuses = Status.all
 	end
 
+	def show
+		set_status
+	end
+
 	def new
 		@status = Status.new
+	end
+
+	def create
+		@status = Status.new(status_params)
+		if @status.save
+			redirect_to root_path
+		else
+			flash[:error] = "Please fill out the form completely"
+			redirect_to root_path
+		end
+	end
+
+	private
+
+	def set_status
+		@status = Status.find(params[:id])
+	end
+
+	def status_params
+		params.require(:status).permit(:dairy, :wheat, :meat, :coffee_cups, :notes, :date, :rating)
 	end
 
 end
