f216afe451815bcae702276570382030229e0a1c
Daniel Fenjves <fenjves.daniel@gmail.com>
2014-05-14 11:58:41 -0400

    added integration tests for lack of cups, rating, and duplicate day

--- spec/features/statuses_spec.rb
+++ spec/features/statuses_spec.rb
@@ -14,6 +14,52 @@ describe "Statuses" do
     	fill_in('Notes', :with => "It was a pretty good day")
     	click_button('Create Status')
     	expect(page).to have_content(7)
+      expect(page).to have_content("Successfully created new daily status")
+    end
+
+    it "should raise an error if a status exists with the same date" do
+      visit(new_status_path)
+      fill_in('status[date]', :with => (Time.now-86400).strftime("%Y-%m-%d"))
+      check('status[wheat]')
+      check('status[dairy]')
+      fill_in('Cups', :with => 2)
+      fill_in('Rating', :with => 7)
+      fill_in('Notes', :with => "It was a pretty good day")
+      click_button('Create Status')
+
+      visit(new_status_path)
+      fill_in('status[date]', :with => (Time.now-86400).strftime("%Y-%m-%d"))
+      check('status[meat]')
+      check('status[dairy]')
+      fill_in('Cups', :with => 4)
+      fill_in('Rating', :with => 3)
+      fill_in('Notes', :with => "This shouldn't work")
+      click_button('Create Status')
+
+      expect(page).to have_content("You already have a daily status for the selected date.")
+    end
+
+    it "should raise an error if coffee cups are not filled out" do
+      visit(new_status_path)
+      fill_in('status[date]', :with => Time.now.strftime("%Y-%m-%d"))
+      check('status[wheat]')
+      check('status[dairy]')
+      fill_in('Rating', :with => 7)
+      fill_in('Notes', :with => "Didn't fill out coffee")
+      click_button('Create Status')
+      expect(page).to have_content("Please add coffee consumption")
+
+    end
+
+    it "should raise an error if daily rating level is not filled out" do
+      visit(new_status_path)
+      fill_in('status[date]', :with => Time.now.strftime("%Y-%m-%d"))
+      check('status[wheat]')
+      check('status[dairy]')
+      fill_in('Cups', :with => 4)
+      fill_in('Notes', :with => "Didn't fill out rating")
+      click_button('Create Status')
+      expect(page).to have_content("Please add a daily rating")
     end
 
   end
