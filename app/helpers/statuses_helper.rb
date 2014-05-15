1300284a03d5e81f3cc2e502b9248abe745c00db
Daniel Fenjves <fenjves.daniel@gmail.com>
2014-05-15 13:52:39 -0400

    implemented info icons on table

--- app/helpers/statuses_helper.rb
+++ app/helpers/statuses_helper.rb
@@ -1,2 +1,15 @@
 module StatusesHelper
+
+	def icon_chooser(status)
+		attributes = ["wheat", "dairy", "meat", "coffee_cups"]
+
+		icons = attributes.collect do |attribute|
+			attribute if (status.send(attribute) && attribute != "coffee_cups")
+		end
+
+		status.send("coffee_cups").times { icons << "coffee_cups"}
+		icons.compact
+	end
+
 end
+
