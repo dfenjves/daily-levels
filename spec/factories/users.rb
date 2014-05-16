5608a6f52cb5ed032c44d305d7502c07c429f0fc
Daniel Fenjves <fenjves.daniel@gmail.com>
2014-05-16 11:17:10 -0400

    added has secure password and appropriate tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,7 @@ FactoryGirl.define do
   factory :user do
     name "Username"
     email "user@user.com"
+    password "foobar"
+    password_confirmation "foobar"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,17 +6,16 @@ describe User do
 	    @user = FactoryGirl.build(:user)
 	  end
 
-		it "responds to name" do
-			expect(@user).to respond_to(:name)
-		end
+	subject { @user }
 
-		it "responds to email" do
-			expect(@user).to respond_to(:email)
-		end
+  it { should respond_to(:name) }
+  it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
-		it "accepts a new valid user" do
-			expect(@user.save).to be_true
-		end
+  it { should be_valid }
 		
 		describe "username issues" do
 			
@@ -79,6 +78,40 @@ describe User do
     		expect(@user).to_not be_valid
     	end
   	end
+
+  	describe "when password is not present" do
+		  before do
+		    @user = User.new(name: "Example User", email: "user@example.com",
+		                     password: " ", password_confirmation: " ")
+		  end
+		  it { should_not be_valid }
+		end
+
+		describe "when password doesn't match confirmation" do
+		  before { @user.password_confirmation = "mismatch" }
+		  it { should_not be_valid }
+		end
+
+		describe "return value of authenticate method" do
+		  before { @user.save }
+		  let(:found_user) { User.find_by(email: @user.email) }
+
+		  describe "with valid password" do
+		    it { should eq found_user.authenticate(@user.password) }
+		  end
+
+		  describe "with invalid password" do
+		    let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+		    it { should_not eq user_for_invalid_password }
+		    specify { expect(user_for_invalid_password).to be_false }
+		  end
+		end
+
+		describe "with a password that's too short" do
+		  before { @user.password = @user.password_confirmation = "a" * 5 }
+		  it { should be_invalid }
+		end
 
 	end
 
