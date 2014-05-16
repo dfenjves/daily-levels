294d2d174124f143276b318e557e5fd7b2e36dec
Daniel Fenjves <fenjves.daniel@gmail.com>
2014-05-16 11:45:42 -0400

    added user controller and initial integration test

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
+get 'users/new', as: 'new_user'
+match '/signup',  to: 'users#new', via: 'get'
+
 root 'statuses#index'
 post 'statuses' => 'statuses#create'
-get 'statuses/new' => 'statuses#new', as: 'new_status' 
-get 'statuses/:id' => 'statuses#show', as: 'status'
+get 'statuses/new' => 'statuses#new', 			as: 'new_status' 
+get 'statuses/:id' => 'statuses#show', 			as: 'status'
 get 'statuses/:id/edit' => 'statuses#edit', as: 'edit_status'
 patch 'statuses/:id' => 'statuses#update'
 put 'statuses/:id' => 'statuses#update'
